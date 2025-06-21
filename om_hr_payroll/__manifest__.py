06240d59245b576d01ea776408aa16def4f995fe
biztinct <30279090+biztinct@users.noreply.github.com>
2025-06-22 08:25:59 +1000

    Addded new modules

---
+++ om_hr_payroll/__manifest__.py
@@ -0,0 +1,55 @@
+# -*- coding:utf-8 -*-
+
+{
+    'name': 'Odoo 16 HR Payroll',
+    'category': 'Generic Modules/Human Resources',
+    'version': '16.0.1.0.2',
+    'sequence': 1,
+    'author': 'Odoo Mates, Odoo SA',
+    'summary': 'Payroll For Odoo 16 Community Edition',
+    'live_test_url': 'https://www.youtube.com/watch?v=0kaHMTtn7oY',
+    'description': "Odoo 16 Payroll, Payroll Odoo 16, Odoo Community Payroll",
+    'website': 'https://www.odoomates.tech',
+    'license': 'LGPL-3',
+    'depends': [
+        'mail',
+        'hr_contract',
+        'hr_holidays',
+        'spreadsheet_oca',
+        'web_notify'
+    ],
+    'data': [
+        'security/hr_payroll_security.xml',
+        'security/ir.model.access.csv',
+        'data/hr_payroll_sequence.xml',
+        'data/hr_payroll_category.xml',
+        'data/hr_payroll_data.xml',
+        'wizard/hr_payroll_payslips_by_employees_views.xml',
+        'views/hr_contract_type_views.xml',
+        'views/hr_contract_views.xml',
+        'views/hr_salary_rule_views.xml',
+        'views/hr_payslip_views.xml',
+        'views/hr_employee_views.xml',
+        'views/hr_payroll_report.xml',
+        'wizard/hr_payroll_contribution_register_report_views.xml',
+        'views/res_config_settings_views.xml',
+        'views/report_contribution_register_templates.xml',
+        'views/report_payslip_templates.xml',
+        'views/report_payslip_details_templates.xml',
+        'views/payslip_portal_templates.xml',
+        'views/hr_contract_history_views.xml',
+        'views/hr_leave_type_view.xml',
+        'views/hr_zoho_staging_views.xml',
+        'views/hr_zoho_views.xml',
+        'data/mail_template.xml',
+    ],
+    'images': ['static/description/banner.png'],
+    'application': True,
+    'assets': {
+        'web.assets_frontend': [
+            'om_hr_payroll/static/src/js/payslip_portal_sidebar.js',
+            'om_hr_payroll/static/src/js/payslip_portal.js',
+        ],
+     },
+
+}
