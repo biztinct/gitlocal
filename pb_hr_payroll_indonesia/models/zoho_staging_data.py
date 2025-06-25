73605ca18be0c756298df07a8e1efe3adf3b9e02
biztinct <30279090+biztinct@users.noreply.github.com>
2025-06-25 17:41:49 +1000

    Indonesia excel updated and now testing

--- pb_hr_payroll_indonesia/models/zoho_staging_data.py
+++ pb_hr_payroll_indonesia/models/zoho_staging_data.py
@@ -6,7 +6,7 @@ from odoo import api, fields, models
 class ZohoStagingData(models.Model):
     _inherit = 'zoho.staging.data'
     
-    # Indonesia specific staging fields
+    # Indonesia specific staging fields (existing)
     gross_pay_idn = fields.Float(string='Gross Pay (IDN)')
     pph21 = fields.Float(string='PPh 21')
     bpjs_kesehatan_employee = fields.Float(string='BPJS Kesehatan - Employee')
@@ -22,6 +22,26 @@ class ZohoStagingData(models.Model):
     npwp_number = fields.Char(string='NPWP Number')
     bpjs_kesehatan_number = fields.Char(string='BPJS Kesehatan Number')
     bpjs_ketenagakerjaan_number = fields.Char(string='BPJS Ketenagakerjaan Number')
+    
+    # ENHANCED FIELDS - NEW ALLOWANCES
+    fixed_allowance_1 = fields.Float(string='Fixed Allowance 1')
+    fixed_allowance_2 = fields.Float(string='Fixed Allowance 2')
+    commission = fields.Float(string='Commission')
+    sign_on_bonus = fields.Float(string='Sign on Bonus')
+    tunjangan_sewa_rumah = fields.Float(string='Tunjangan Sewa Rumah')
+    tunjangan_duka = fields.Float(string='Tunjangan Duka')
+    tunjangan_suka = fields.Float(string='Tunjangan Suka')
+    severance_appreciation = fields.Float(string='Severance/ Appreciation')
+    lain_lain_allowance = fields.Float(string='Lain-lain (Allowance)')
+    
+    # ENHANCED FIELDS - NEW DEDUCTIONS
+    deduction_1 = fields.Float(string='Deduction 1')
+    deduction_2 = fields.Float(string='Deduction 2')
+    deduction_3 = fields.Float(string='Deduction 3')
+    koperasi = fields.Float(string='Koperasi')
+    pinjaman = fields.Float(string='Pinjaman')
+    cicilan = fields.Float(string='Cicilan')
+    lain_lain_deduction = fields.Float(string='Lain-lain (Deduction)')
 
 
 class ZohoStagingImporter(models.TransientModel):
@@ -38,6 +58,26 @@ class ZohoStagingImporter(models.TransientModel):
                 'bpjs_kesehatan_number': zoho_employee.get('BPJS_Kesehatan_Number', ''),
                 'bpjs_ketenagakerjaan_number': zoho_employee.get('BPJS_Ketenagakerjaan_Number', ''),
                 'payroll_country': 'ID',
+                
+                # ENHANCED ALLOWANCES
+                'fixed_allowance_1': zoho_employee.get('fixed_allowance_1', 0),
+                'fixed_allowance_2': zoho_employee.get('fixed_allowance_2', 0),
+                'commission': zoho_employee.get('commission', 0),
+                'sign_on_bonus': zoho_employee.get('sign_on_bonus', 0),
+                'tunjangan_sewa_rumah': zoho_employee.get('tunjangan_sewa_rumah', 0),
+                'tunjangan_duka': zoho_employee.get('tunjangan_duka', 0),
+                'tunjangan_suka': zoho_employee.get('tunjangan_suka', 0),
+                'severance_appreciation': zoho_employee.get('severance_appreciation', 0),
+                'lain_lain_allowance': zoho_employee.get('lain_lain_allowance', 0),
+                
+                # ENHANCED DEDUCTIONS
+                'deduction_1': zoho_employee.get('deduction_1', 0),
+                'deduction_2': zoho_employee.get('deduction_2', 0),
+                'deduction_3': zoho_employee.get('deduction_3', 0),
+                'koperasi': zoho_employee.get('koperasi', 0),
+                'pinjaman': zoho_employee.get('pinjaman', 0),
+                'cicilan': zoho_employee.get('cicilan', 0),
+                'lain_lain_deduction': zoho_employee.get('lain_lain_deduction', 0),
             })
         
         return vals
