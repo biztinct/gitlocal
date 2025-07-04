138e84770952c29ba89340828fb194347f00055c
biztinct <30279090+biztinct@users.noreply.github.com>
2025-07-04 11:50:20 +1000

    New module refactoring in progress

---
+++ pb_hr_payroll_base/models/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+from . import payroll_dashboard_base
+from . import payroll_country_selector
+from . import zoho_base_models
+from . import hr_payroll_structure_base
---
+++ pb_hr_payroll_base/wizards/__init__.py
@@ -0,0 +1,2 @@
+# -*- coding: utf-8 -*-
+from . import country_selector_wizard
