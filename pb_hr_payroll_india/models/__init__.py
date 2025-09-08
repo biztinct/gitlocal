947ab4132c50c8ab9b811bf47f6426ff0b055da2
biztinct <30279090+biztinct@users.noreply.github.com>
2025-09-09 06:17:12 +1000

    Indi specific

--- pb_hr_payroll_india/models/__init__.py
+++ pb_hr_payroll_india/models/__init__.py
@@ -2,6 +2,7 @@
 
 from . import hr_payroll_structure
 from . import hr_contract
+from . import hr_payslip  # NEW: India-specific payslip processing
 from . import zoho_staging_data
 from . import zoho_employee_data  # NEW: India employee data extensions
 from . import gratuity_payment
