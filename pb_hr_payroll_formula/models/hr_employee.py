9e88c9a3cbeb61f03112afa88ed73f5872ed4db2
biztinct <30279090+biztinct@users.noreply.github.com>
2026-01-12 21:51:27 +1100

    .

--- pb_hr_payroll_formula/models/hr_employee.py
+++ pb_hr_payroll_formula/models/hr_employee.py
@@ -10,3 +10,6 @@ class HrEmployee(models.Model):
     position_name = fields.Char(string='Position')
     job_title_text = fields.Char(string='Job Title')
     date_of_joining = fields.Date(string='Date of Joining')
+    overtime_status = fields.Char(string='Overtime Status', default='')
+    fire_prevention_officer = fields.Char(string='Fire Prevention Officer', default='')
+    tham_gia_bhxh = fields.Char(string='Tham gia BHXH', default='')
